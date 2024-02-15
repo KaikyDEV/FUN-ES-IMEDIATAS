1b27cab2f96017e890a666fb28985ac045590084
Kaiky <126119808+KaikyDEV@users.noreply.github.com>
2024-02-15 13:38:22 -0300

    Add files via upload

---
+++ aula02.js
@@ -0,0 +1,16 @@
+function escrevaMeuNome(nome){
+    console.log('Meu nome é ' + nome);
+}
+
+escrevaMeuNome('Kaiky');
+
+function verificarIdade(idade){
+    if(idade >= 18){
+        console.log('Você é maior de idade')
+    }
+    else{
+        console.log('Você é menor de idade')
+    }
+}
+
+verificarIdade(18);
